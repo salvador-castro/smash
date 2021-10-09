753867af0fff9c41aab3f2c4e93c551624d7f022
Salvador Castro <salvacastro06@gmail.com>
2021-10-09 10:42:20 -0300

    Actualizacion countdown

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "September 28 2021",
+	date: "October 11 2021",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
